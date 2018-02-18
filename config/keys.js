551d3b7480b981434e55367d648ef8443d60c2af
Kar3n <atkaa@gmail.com>
2018-02-18 16:26:30 +0400

    'all'

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+	mongoURI: 'mongodb://atkaa92:kar6670929497@ds237868.mlab.com:37868/storybooks-92',
+	googleClientID: '232828126851-it8023elqt9i1hk12ahho9ujvjpjroks.apps.googleusercontent.com',
+	googleClientSecret: 'TtU1TDZ96RMi2Ks7cDqRIcI9'
+}
